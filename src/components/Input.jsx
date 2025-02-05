0f244935a972e1753b55dd5e29aaa090be3b37f5
simranq <qsimran08@gmail.com>
2025-02-06 01:15:44 +0530

    m

---
+++ src/components/Input.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function Input() {
+  // Declare a state variable for the input text
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Type something..."
+      />
+      <p>You typed: {text}</p>
+    </div>
+  );
+}
+
+export default Input;
